7e3ada8b7e72c3fe37ecac8db636993e8afbbdde
Bugi Abdulkarim <bugrahanabdulkarim@gmail.com>
2018-10-01 14:24:20 -0400

    user model added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    seller_status = models.BooleanField(default=True)
+    email = models.CharField(max_length=30)
+    university = models.CharField(max_length=100)
