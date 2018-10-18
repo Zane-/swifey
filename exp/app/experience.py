066a69044db5a754aad6e5f6b13dc102f0e0f755
Zane Bilous <zanebilous@gmail.com>
2018-10-18 13:43:55 -0400

    Fix url for accessing model-api

--- exp/app/experience.py
+++ exp/app/experience.py
@@ -1,31 +1,34 @@
 import os
 import requests
 
-os.environ['NO_PROXY'] = '127.0.0.1'
 
 def get(model, num):
-    url = 'http://127.0.0.1:8001/api/v1/{}/{}/'.format(model, num)
+    url = 'http://models-api:8001/api/v1/{}/{}/'.format(model, num)
     req = requests.get(url)
     req.raise_for_status()
     return req.json()
 
 def post(model,json):
-    url = 'http://127.0.0.1:8001/api/v1/{}/'.format(model)
+    url = 'http://models-api:8001/api/v1/{}/'.format(model)
     req = requests.post(url, data=json)
     req.raise_for_status()
     # if no exception was raised req was ok
-    return 'OK'
 
 def put(model, num, json):
-    url = 'http://127.0.0.1:8001/api/v1/{}/{}/'.format(model, num)
+    url = 'http://models-api:8001/api/v1/{}/{}/'.format(model, num)
     req = requests.put(url, data=json)
     req.raise_for_status()
     # if no exception was raised req was ok
-    return 'OK'
 
 def del(model, num):
-    url = 'http://127.0.0.1:8001/api/v1/{}/{}/'.format(model, num)
+    url = 'http://models-api:8001/api/v1/{}/{}/'.format(model, num)
     requests.delete(url)
     req.raise_for_status()
     # if no exception was raised req was ok
-    return 'OK'
+
+def get_all(model):
+    url = 'http://models-api:8000/api/v1/{}/'.format(model)
+    requests.get(url)
+    req.raise_for_status()
+    json = req.json['objects']
+    return json
