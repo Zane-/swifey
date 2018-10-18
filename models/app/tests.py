cb2809dcbf4a3f2a5d5c6052bc5951937e2de167
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-10-18 10:33:46 -0400

    changed created_by back to integer val

--- models/app/tests.py
+++ models/app/tests.py
@@ -10,7 +10,7 @@ class UserTestCase(TestCase):
         pass
 
     # redirect client from home to details page
-    def redirect_to_details(self):
+    def test_redirect_to_details(self):
 
     # tear down
     def tearDown(self):
@@ -24,15 +24,23 @@ class ItemTestCase(TestCase):
 
     # test if label is 's' or 'm'
     def test_label(self):
+        self.assertIn(Item.label, ('s', 'm'))
 
     # test if number of swipes is greater than 1
     def test_num_swipes(self):
+        self.assertGreaterEqual(Item.num_swipes, 0)
 
     # test if cost of swipe is greater than 1
     def test_cost(self):
+        self.assertGreaterEqual(Item.cost, 0)
 
     # test that seller is not client and valid user
     def test_owner(self):
+        self.assertNotEqual(Item.created_by.split(),
+                            [User.first_name, User.last_name])
+        # if separator is not a string then it failts
+        with self.assertRaises(TypeError):
+            Item.created_by.split(2)
 
     # tear down
     def tearDown(self):
