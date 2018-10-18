1142be2a4ffbd9b1fb6113ae2586c01f8514b480
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-10-18 07:52:16 -0400

    general outline for test cases created

--- exp/app/tests.py
+++ exp/app/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from swifey.models import User, Item
 
 # Create your tests here.
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+
+    def success_response(self):
+
+    def fails_invalid(self):
+
+    def tearDown(self):
+
+class ItemTestCase(TestCase):
+
+    def setUp(self):
+
+    def success_response(self):
+
+    def fails_invalid(self):
+
+    def tearDown(self):
+
