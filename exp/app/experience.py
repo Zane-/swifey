5f6527d150b676c3dae7a05549f52db30e4a608e
Zane Bilous <zanebilous@gmail.com>
2018-10-18 14:57:06 -0400

    Bug fixes

--- exp/app/experience.py
+++ exp/app/experience.py
@@ -21,27 +21,30 @@ def put(model, num, json):
 
 def del(model, num):
     url = 'http://models-api:8001/api/v1/{}/{}/'.format(model, num)
-    requests.delete(url)
+    req = requests.delete(url)
     req.raise_for_status()
     # if no exception was raised req was ok
 
 def get_all(model):
-    url = 'http://models-api:8000/api/v1/{}/'.format(model)
-    requests.get(url)
+    url = 'http://models-api:8001/api/v1/{}/'.format(model)
+    req = requests.get(url)
     req.raise_for_status()
-    json = req.json['objects']
+    json = req.json()['objects']
+    # replace created_by id with actual user_data
+    for item in json:
+        user_id = item['created_by']
+        user_data = get(user, user_id)
+        item['created_by'] = user_data
     return json
 
 def get_trades(*, for_swipes, sort=None):
-    url = 'http://models-api:8000/api/v1/item/'
-    requests.get(url)
-    req.raise_for_status()
-    json = req.json['objects']
-    for_swipes_trades = [d for d in objects if d['for_swipes'] == for_swipes]
+    trades = [d for d in get_all(item) if d['for_swipes'] == for_swipes]
 
+    # sort by ascending/descending num_swipes values
     if sort == 'high':
         for_swipes_trades.sort(key=lambda d: d['num_swipes'], reverse=True)
     elif sort == 'low':
         for_swipes_trades.sort(key=lambda d: d['num_swipes'])
 
     return for_swipes_trades
+
