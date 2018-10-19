8d7d6d6f29a7b46e2277c2dd9c4ea9764821dab5
Zane Bilous <zanebilous@gmail.com>
2018-10-18 21:13:32 -0400

    Add model and api tests

--- models/app/tests.py
+++ models/app/tests.py
@@ -1,44 +1,131 @@
 from django.test import TestCase
 from .models import User, Item
+from tastypie.test import TestApiClient
 
-# Create your tests here.
+import json
+
+# set up api client
+global client
+client = TestApiClient()
 
 class UserTestCase(TestCase):
-
-    # start up
     def setUp(self):
-        pass
+        User.objects.create(
+            first_name='Zane',
+            last_name='Bilous',
+            seller_status='True',
+            email='zanebilous@gmail.com',
+            university='University of Virginia'
+        )
+        User.objects.create(
+            first_name='Sohan',
+            last_name='Kabiraj',
+            seller_status='True',
+            email='sohan@gmail.com',
+            university='University of Virginia'
+        )
+        User.objects.create(
+            first_name='Bugi',
+            last_name='Abdulkarim',
+            seller_status='True',
+            email='bugi@gmail.com',
+            university='University of Virginia'
+        )
 
-    # redirect client from home to details page
-    def test_redirect_to_details(self):
+    def testGet(self):
+        response = client.get('/api/v1/user/1/')
+        response = response.content.decode('ascii')
+        d = json.loads(response)
+        self.assertEqual(d['first_name'], 'Zane')
 
-    # tear down
-    def tearDown(self):
-        pass
+    def testPost(self):
+        response = client.post('/api/v1/user/', data={
+            'first_name': 'Bugi',
+            'last_name': 'Abdulkarim',
+            'seller_status': False,
+            'email': 'bugi@virginia.edu',
+            'university': 'University of Virginia'
+        })
+        self.assertEqual(response.status_code, 201)
+
+    def testPut(self):
+        response = client.put('/api/v1/user/1/', data={
+            'first_name': 'Zane',
+            'last_name': 'Bilous',
+            'seller_status': True,
+            'email': 'zanebilous@virginia.edu',
+            'university': 'University of Virginia'
+        })
+        self.assertEqual(response.status_code, 201)
+        response = client.get('/api/v1/user/1/')
+        response = response.content.decode('ascii')
+        d = json.loads(response)
+        self.assertEqual(d['email'], 'zanebilous@virginia.edu')
+
+    def testZDelete(self):
+        response = client.delete('/api/v1/user/1/')
+        self.assertEqual(response.status_code, 404)
+
 
 class ItemTestCase(TestCase):
-
-    # start up
     def setUp(self):
-        pass
+        Item.objects.create(
+            title="Looking for futon",
+            description="Will trade 30 swipes for a used futon",
+            created_by=2,
+            num_swipes=30,
+            label="S",
+            for_swipes=False
+        )
+        Item.objects.create(
+            title="Looking for futon",
+            description="Will trade 30 swipes for a used futon",
+            created_by=2,
+            label="S",
+            num_swipes=30,
+            for_swipes=False
+        )
+        Item.objects.create(
+            title="Looking for futon",
+            description="Will trade 30 swipes for a used futon",
+            created_by=2,
+            num_swipes=30,
+            label="S",
+            for_swipes=False
+        )
 
-    # test if label is 's' or 'm'
-    def test_label(self):
-        self.assertIn(Item.label, ('s', 'm'))
+    def testGet(self):
+        response = client.get('/api/v1/item/1/')
+        response = response.content.decode('ascii')
+        d = json.loads(response)
+        self.assertEqual(d['title'], 'Looking for futon')
 
-    # test if number of swipes is greater than 1
-    def test_num_swipes(self):
-        self.assertGreaterEqual(Item.num_swipes, 0)
+    def testPost(self):
+        response = client.post('/api/v1/item/', data={
+            "title": "Looking for futon",
+            "description": "Will trade 30 swipes for a used futon",
+            "created_by": 2,
+            "num_swipes": 30,
+            "label": "S",
+            "for_swipes": False
+        })
+        self.assertEqual(response.status_code, 201)
 
-    # test if cost of swipe is greater than 1
-    def test_cost(self):
-        self.assertGreaterEqual(Item.cost, 0)
+    def testPut(self):
+        response = client.put('/api/v1/item/1/', data={
+            "title": "Looking for futon",
+            "description": "Will trade 30 swipes for a used futon",
+            "created_by": 2,
+            "num_swipes": 40,
+            "label": "S",
+            "for_swipes": False
+        })
+        self.assertEqual(response.status_code, 201)
+        response = client.get('/api/v1/item/1/')
+        response = response.content.decode('ascii')
+        d = json.loads(response)
+        self.assertEqual(d['num_swipes'], 40)
 
-    # test that seller is not client and valid user
-    def test_owner(self):
-        self.assertNotEqual(Item.created_by,
-                            [])
-
-    # tear down
-    def tearDown(self):
-        pass
+    def testZDelete(self):
+        response = client.delete('/api/v1/item/1/')
+        self.assertEqual(response.status_code, 404)
--- web/web/settings.py
+++ web/web/settings.py
@@ -121,8 +121,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    '../app/static/',
-]
-
 STATIC_URL = '/static/'
