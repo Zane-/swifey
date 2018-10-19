7116cb374ad2f22b1510b5bef3ebbe37a2b725f5
Zane Bilous <zanebilous@gmail.com>
2018-10-18 22:16:07 -0400

    Fix json

--- web/app/views.py
+++ web/app/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request, 'app/home.html', {})
 
 def details(request):
-    for_swipes = requests.get('http://exp-api:8000/api/trades/for_swipes/')
-    for_items = requests.get('http://exp-api:8000/api/trades/for_items/')
+    for_swipes = requests.get('http://exp-api:8000/api/trades/for_swipes/').json()
+    for_items = requests.get('http://exp-api:8000/api/trades/for_items/').json()
     return render(request, 'app/details.html',
         {'for_swipes': for_swipes, 'for_items': for_items})
