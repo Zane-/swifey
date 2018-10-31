300b4c5fd10165b9a4cee200bb283eee52c33ab4
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-10-31 08:48:53 -0400

    test cases for new models

--- models/app/tests.py
+++ models/app/tests.py
@@ -1,8 +1,25 @@
 from django.test import TestCase
-from .models import User, Listing
+from .models import Authenticator, User, UserForm,
+                    Listing, ListingForm
 
 import json
+
+class AuthTestCase(TestCase):
+    def setUp(self):
+        pass
 
 class UserTestCase(TestCase):
     def setUp(self):
         pass
+
+class UserFormTestCase(TestCase):
+    def setUp(self):
+        pass
+
+class ListingTestCase(TestCase):
+    def setUp(self):
+
+class ListingFormTestCase(TestCase):
+    def setUp(self):
+        pass
+
