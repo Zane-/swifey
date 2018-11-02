158da8a4f28fcbd3e26403d28426a8c30b4c27e3
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-11-02 13:22:47 -0400

    adding new views for pages

--- web/app/views.py
+++ web/app/views.py
@@ -10,3 +10,13 @@ def details(request):
     for_items = requests.get('http://exp-api:8000/api/trades/for_items/').json()
     return render(request, 'app/details.html',
         {'for_swipes': for_swipes, 'for_items': for_items})
+
+def login(request):
+    return render(request, 'app/login.html', {})
+
+def sign_up(request):
+    return render(request, 'app/sign_up.html', {})
+
+def new_listing(request):
+    return render(request, 'app/new_listing.html', {})
+
