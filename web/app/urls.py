a455e90c7b4acfdfb3d1f39b8f7d6719f34d5b1a
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-11-02 13:27:04 -0400

    urls for views added

--- web/app/urls.py
+++ web/app/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url 
+from django.conf.urls import url
 from . import views
 from django.urls import path
-    
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('details', views.details, name='details'),
+    path('login', views.login, name='login'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('new_listing', views.new_listing, name='new_listing')
 ]
