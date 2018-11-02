faf85ba250e33195452cefcc4dca999133a3bf20
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-11-02 15:07:16 -0400

    initial steps for setting up web views

--- web/app/views.py
+++ web/app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 import requests
+
+# :TODO import UserForm, ListingForm from models/app/models.py
 
 def home(request):
     return render(request, 'app/home.html', {})
@@ -12,11 +15,46 @@ def details(request):
         {'for_swipes': for_swipes, 'for_items': for_items})
 
 def login(request):
-    return render(request, 'app/login.html', {})
+    auth = request.COOKIES.get('auth')
+    # Direct to home page if auth token is validated
+    if auth:
+        return HttpResponseRedirect(reverse('home'))
+    # Return new login form
+    if request.method == 'GET':
+        return render(request, 'app/login.html', {'form': form, 'auth': auth })
+    # :TODO POST valid request
+    # :TODO handle invalid POST request
+    # :TODO grab auth token once post request was made successfully
+    token = ''
+    next = HttpResponseRedirect(reverse('home'))
+    next.set_cookie('auth', token)
+    return next
+
+def logout(request):
+    auth = request.COOKIES.get('auth')
+    if not auth:
+        return HttpResponseRedirect(reverse('login'))
+    # :TODO validate logout with backend
+    next = HttpResponseRedirect(reverse('index'))
+    next.delete_cookie('auth')
+    return next
 
 def sign_up(request):
-    return render(request, 'app/sign_up.html', {})
+    auth = request.COOKIES.get('auth')
+    # Direct to home page if auth token is validated
+    if auth:
+        return HttpResponseRedirect(reverse('index'))
+    # Return new sign up form
+    if request.method == 'GET':
+        return render(request, 'app/sign_up.html', {'form': UserForm,'auth': auth })
+    # :TODO POST valid request
+    # :TODO handle invalid POST request
+    # :TODO grab auth token once post request was made successfully
+    # :TODO handle next steps after valid POST request
+    return render(request, 'app/sign_up.html', {'form': , 'auth': auth })
 
 def new_listing(request):
-    return render(request, 'app/new_listing.html', {})
+    auth = request.COOKIES.get('auth')
+    # :TODO handle new listing logic
+    return render(request, 'app/new_listing.html', {'form': , 'auth': auth })
 
