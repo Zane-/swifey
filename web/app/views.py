ffd6948512ba0554c5e4829d9ff304e35bb00b00
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-11-14 17:51:57 -0500

    rewrote views for everything

--- web/app/views.py
+++ web/app/views.py
@@ -7,41 +7,42 @@ from .forms import LoginForm, SignupForm, ListingForm
 def home(request):
     return render(request, 'app/index.html', {})
 
+
 def details(request):
     pass
 
+
 def login(request):
-    form = LoginForm()
     auth = request.COOKIES.get('auth')
     warning = "You have entered an invalid username or password!"
     # Direct to home page if auth token is validated
     if auth:
-        return HttpResponseRedirect(reverse('home'))
-    # return new login form
-    if request.method == 'GET':
-        return render(request, 'app/login.html', {'form': form, 'auth': auth })
-    # POST valid request
-    f = LoginForm(request.POST)
-    # handle invalid POST request
-    if not f.is_valid():
-        # invalid form, return to login
-        return render(request, 'app/login.html', {'form': form, 'err': warning })
-    # clean username and password fields
-    email = f.cleaned_data['email']
-    password = f.cleaned_data['password']
-    # send validated information to experience layer
-    response = requests.post('http://exp-api:8000/login/', data=request.post)
-    # Experience layer checks if invalid information was provided
-    if not response or not response['OK']:
-        # return to login page with error message
-        return render(request, 'app/login.html', { 'form': form, 'err': warning })
-    """ If we made it here, we can log them in. """
-    # grab auth token once post request was made successfully
-    # i.e. token = response['authenticator'] or whatever field
-    # name is for auth token from the response
-    next = HttpResponseRedirect(reverse('home'))
-    next.set_cookie('auth', response.json())
-    return next
+        return HttpResponseRedirect(reverse('index'))
+    if request.method == 'POST':
+        form = Login(request.POST)
+        # handle valid POST request
+        if form.is_valid():
+            data = {
+                'email': form.cleaned_data['email'],
+                'password': form.cleaned['password']
+            }
+            # POST request to experience layer with data from form
+            req = requests.post('http://exp-api:8000/login/', data=data)
+            if req.status_code == '200':
+                """ If we made it here, we can log them in. """
+                response = HttpResponseRedirect(reverse('index'))
+               # set the auth cookie using the json response from POST request
+                response.set_cookie('auth', req.json())
+                return response
+        # handle invalid POST request
+        else:
+            # invalid form, return to login
+            return render(request, 'app/login.html', {'form': form, 'err': warning})
+    else:
+        form = LoginForm()
+
+    return render(request, 'app/form.html', { 'form': form })
+
 
 def logout(request):
     auth = request.COOKIES.get('auth')
@@ -54,68 +55,84 @@ def logout(request):
     # POST logout and validate request
     return next
 
+
 def sign_up(request):
-    form = UserForm()
     auth = request.COOKIES.get('auth')
-    warning = "Invalid! Please fill out all fields appropriately."
+    warning = "Invalid! Please fill out all the fields appropriately."
     # Direct to home page if auth token is validated
     if auth:
-        return HttpResponseRedirect(reverse('home'))
-    # Return new sign up form
-    if request.method == 'GET':
-        return render(request, 'app/sign_up.html', {'form': form, 'auth': auth })
-    # POST valid request
-    f = UserForm(request.POST)
-    # handle invalid POST request
-    if not f.is_valid():
-        # invalid form, return to sign_up
-        return render(request, 'app/sign_up.html', { 'form': form, 'auth': auth, 'err': warning })
-    # clean up fields of UserForm
-    first_name = f.cleaned_data['first_name']
-    last_name = f.cleaned_data['last_name']
-    email = f.cleaned_data['email']
-    password = f.cleaned_data['password']
-    university = f.cleaned_data['university']
-    has_meal_plan = f.cleaned_data['has_meal_plan']
-    # send validated information to experience layer
-    response = requests.post('http://exp-api:8000/signup/', data=request.post)
-    # Experience layer checks if invalid information was provided
-    if not response or not response['OK']:
-        # return to login page with error message
-        return render(request, 'app/sign_up.html', { 'form': form, 'err': warning })
-    next = HttpResponseRedirect(reverse('login'))
-    return next
+        return HttpResponseRedirect(reverse('index'))
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        # handle valid POST request
+        if form.is_valid():
+            data = {
+                'first_name': form.cleaned_data['first_name'],
+                'last_name': form.cleaned_data['last_name'],
+                'email': form.cleaned_data['email'],
+                'password': form.cleaned_data['password'],
+                'university': form.cleaned_data['university'],
+                'has_meal_plan': form.cleaned_data['has_meal_plan'],
+            }
+            req = requests.post('http://exp-api:8000/signup/', data=data)
+            if req.status_code == '201':
+                """ If we made it here, we can sign them up. """
+                response = HttpResponseRedirect(redirect('index'))
+               # set the auth cookie using the json response from POST request
+                response.set_cookie('auth', req.json())
+                return response
+        else:
+            # invalid form, return to sign up
+            return render(request, 'app/signup.html', { 'form': form, 'err': warning })
+    else:
+        form = SignupForm()
+
+    return render(request, 'app/form.html', { 'form': form })
+
 
 def new_listing(request):
-    form = ListingForm()
     auth = request.COOKIES.get('auth')
-    warning = "Invalid! Please fill out all fields appropriately."
-    # redirect to login page if there is no auth cookie or it is more than a week old
-    auth_check = requests.post('http://exp-api:8000/validate_auth/', data=auth)
-    auth_valid = auth_check.status_code == 200
-    if not auth or not auth_valid:
-        return HttpResponseRedirect(redirect('login') + '?next=' + reverse('new_listing'))
-    # return new listing form
-    if request.method == 'GET':
-        return render(request, 'app/new_listing.html', { 'form': form, 'auth': auth })
-    # POST valid request
-    f = ListingForm(request.POST)
+    warning = "Invalid! Please fill out all the fields appropriately."
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        # handle valid POST request
+        if form.is_valid():
+            data = {
+                'title': form.cleaned_data['title'],
+                'description': form.cleaned_data['description'],
+                'listing_type': form.cleaned_data['listing_type'],
+                'num_swipes': form.cleaned_data['num_swipes'],
+            }
+            req = requests.post('http://exp-api:8000/new_listing/', data=data)
+            if req.status_code == '201':
+                """ If we made it here, we can sign them up. """
+                response = HttpResponseRedirect(redirect('index'))
+                return response
+        else:
+            # invalid form, return to sign up
+            return render(request, 'app/new_listing.html', { 'form': form, 'err': warning })
+    else:
+        form = ListingForm()
+
+    return render(request, 'app/form.html', { 'form': form })
+
+
+# def search(request):
+    # if request.method == 'GET':
+        # grab text field from search form
+        # return render(request, 'app/search.html', {'form': form })
+    # f = SearchForm(request.POST)
+
     # handle invalid POST request
-    if not f.is_valid():
-        # invalid form, return to listing form
-        return render(request, 'app/new_listing.html', { 'form': form, 'auth': auth, 'err': warning })
-    # clean up fields of ListingForm
-    title = f.cleaned_data['title']
-    description = f.cleaned_data['description']
-    listing_type = f.cleaned_data['listing_type']
-    num_swipes = f.cleaned_data['num_swipes']
-    # send validated information to experience layer
-    response = requests.pos('http://exp-api:8000/create_listing/', data=request.post)
+    # if not f.is_valid(():
+        # invalid form
+        # return render(request, 'app/search.html', { 'form': form, 'err': warning })
+    # clean up fields of SearchForm
+    # search = f.cleaned_data['search']
+    # send valid information to experience layer
+    # response = requests.post('htps://exp-api:8000/search/', data=request.post)
     # Experience layer checks if invalid information was provided
-    if not response or not response['OK']:
-        # OPTIONAL :TODO check if experience layer reports invalid authenticator
-        # use if statement then indent the rest
-        # return to login page with error message
-        return render(request, 'app/login.html', { 'form': form, 'err': warning })
-    """ If we made it here, we have succesfully created listing. """
-    return HttpResponseRedirect(reverse('home'))
+    # if not response or not response['OK']:
+    #     return render(reqest, 'app/search.html', { 'form': form, 'err': warning })
+    # """ If we made it here, we have successfully searched. """
+    # return HttpResponseRedirect(reverse('search_result'))
