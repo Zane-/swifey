afa5c0eba3f4fcbe93eded1c7924bc8cd9d54b09
babdulkarim <bugrahanabdulkarim@gmail.com>
2018-11-14 16:30:13 -0500

    project 5

---
+++ batch/batch.py
@@ -0,0 +1,14 @@
+from kafka import KafkaProducer
+import json
+from elasticsearch import Elasticsearch
+
+consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+es = Elasticsearch(['es'])
+
+
+while True:
+    for message in consumer:
+        new_listing = json.loads((message.value).decode('utf-8'))
+        es.index(index='listing_index', doc_type='listing', id=new_listing['id'], body=new_listing)
+        es.indices.refresh(index="listing_index")
+
