711eca14d2f3cb2516bba9e24be90715cb6ed198
Zane Bilous <zanebilous@gmail.com>
2018-11-14 11:43:40 -0500

    Fix dictionary key

--- web/app/auth.py
+++ web/app/auth.py
@@ -4,9 +4,10 @@ def is_valid_auth(cookie):
     """Returns whether or not an auth cookie is valid.
     The cookie is valid if it is less than a week old and the
     user id matches the authenticator object in the Authenticator table."""
-    if not cookie['auth']:
+    auth = cookie.get('auth')
+    if not auth:
         return False
-    req = requests.post('http://exp-api:8000/api/validate_auth/', data=cookie['auth'])
+    req = requests.post('http://exp-api:8000/api/validate_auth/', data=auth)
     if req.status_code == 200:
         return True
     else:
