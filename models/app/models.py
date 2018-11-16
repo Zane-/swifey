04604949f3cc6508206244e1df4002c16343d424
Zane Bilous <zanebilous@gmail.com>
2018-11-15 23:21:49 -0500

    Revert "Update User model"
    This reverts commit 6b474465a8b44f280757249917bb81aaea4eb91a.

--- models/app/models.py
+++ models/app/models.py
@@ -3,7 +3,6 @@ import hmac
 import json
 import os
 
-from django.conf import settings
 from django.contrib.auth.hashers import make_password, check_password
 from django.core.validators import validate_email, RegexValidator
 from django.db import models
@@ -23,9 +22,9 @@ class Authenticator(models.Model):
     def generate_auth(self):
         """Generates a 256 bit random authentication bitstring."""
         auth = hmac.new(
-            key=settings.SECRET_KEY.encode('utf-8'),
+            key = settings.SECRET_KEY.encode('utf-8'),
             msg=os.urandom(32),
-            digestmod='sha256',
+            digestmod = 'sha256',
         ).hexdigest()
         # if auth is already in the db, recurse and generate a new one
         if self.auth_exists(auth):
@@ -54,11 +53,42 @@ class Authenticator(models.Model):
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField()
-    password = models.CharField()
+    email = models.CharField(
+        max_length=30,
+        validators=[
+            validate_email,
+            RegexValidator(
+                regex=r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.edu$)',
+                message='Please use a valid .edu email'
+            )
+        ]
+    )
+    password = models.CharField(max_length=256)
     university = models.CharField(max_length=100)
     has_meal_plan = models.BooleanField(default=False)
     date_joined = models.DateField(auto_now_add=True)
+    listings = models.TextField()
+
+    def get_listings(self):
+        """
+        Gets the listings into python list form so we may modify it.
+        Listings are stored as a list of primary keys to listing objects
+        represented as json text in a TextField.
+        """
+        return json.loads(self.listings)
+
+    def set_listings(self, listings):
+        """
+        Sets the listings field by converting the listings list
+        into a string. You must call user.save() on the object
+        to save the changes in the database.
+
+        Example usage:
+            listings = user.get_listings()
+            listings.append(listing.id)
+            user.set_listings(listings)
+        """
+        self.listings = json.dumps(listings)
 
     def json(self):
         """Returns a python dictionary of field values."""
@@ -70,6 +100,9 @@ class User(models.Model):
             'university': self.university,
             'has_meal_plan': self.has_meal_plan,
             'date_joined': self.date_joined,
+            # results to: takes 1 positional argument but 2 were given error
+            # 'listings': self.get_listings(self.listings)
+            'listings': self.listings
         }
 
     def __str__(self):
@@ -87,6 +120,9 @@ class UserForm(ModelForm):
             'university',
             'has_meal_plan',
         ]
+        widgets = {
+                'password': PasswordInput(),
+        }
 
 
 class Listing(models.Model):
