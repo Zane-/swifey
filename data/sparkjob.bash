78d613206e0c7017acdc1d6877273a655f80d76c
Boogs <bugrahanabdulkarim@gmail.com>
2018-12-11 19:02:25 -0500

    bash script to automate spark job & install dependencies

---
+++ data/sparkjob.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+apt-get update &&
+apt-get install python3-dev libmysqlclient-dev -y &&
+apt-get install python-pip -y &&
+pip install mysqlclient &&
+apt-get install python-mysqldb
+
+while true
+do
+	python ./mapreduce.py
+	sleep 60
+done
