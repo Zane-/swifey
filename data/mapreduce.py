3a5507dd8d01c213e9c60039ae918059673d5e13
Boogs <bugrahanabdulkarim@gmail.com>
2018-12-13 15:03:51 -0500

    reference steps of algorithm by number as outlined

--- data/mapreduce.py
+++ data/mapreduce.py
@@ -4,17 +4,17 @@ from itertools import combinations
 # A pseudocode map-reduce style algorithm for computing co-views is
 # something like:
 #
-# Read data in as pairs of (user_id, item_id clicked on by the user)
-# Group data into (user_id, list of item ids they clicked on)
-# Transform into (user_id, (item1, item2) where item1 and item2 are
+# 1. Read data in as pairs of (user_id, item_id clicked on by the user)
+# 2. Group data into (user_id, list of item ids they clicked on)
+# 3. Transform into (user_id, (item1, item2) where item1 and item2 are
 # pairs of items the user clicked on
-# Transform into ((item1, item2), list of user1, user2 etc) where users
+# 4. Transform into ((item1, item2), list of user1, user2 etc) where users
 # are all the ones who co-clicked (item1, item2)
-# Transform into ((item1, item2), count of distinct users who co-clicked
+# 5. Transform into ((item1, item2), count of distinct users who co-clicked
 # (item1, item2)
-# Filter out any results where less than 3 users co-clicked the same
+# 6. Filter out any results where less than 3 users co-clicked the same
 # pair of items
-# bring the data back to the master node so we can print it out
+# 7. bring the data back to the master node so we can print it out
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
@@ -29,10 +29,11 @@ pairs = data.map(lambda line: line.split("\t"))
 # our implementation of algorithm using some spark operations on
 # RDD/iter tools
 
-# create dataset of (K, Iterable<V>) pairs
+
+# 1. create dataset of (K, Iterable<V>) pairs
 group = pairs.groupByKey()
 
-# map user id to multiple pairs of [user id + item visited]
+# 2. map user id to multiple pairs of [user id + item visited]
 group_pairs = group.flatMap(lambda items: [(items[0], pair) for pair
                                 in combinations(items[1], 2)])
 
