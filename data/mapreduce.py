1bd524d0cc840b794493e0a051cf3b3a1aac8fcf
Boogs <bugrahanabdulkarim@gmail.com>
2018-12-13 16:09:11 -0500

    update on spark job

--- data/mapreduce.py
+++ data/mapreduce.py
@@ -36,20 +36,31 @@ count = pages.reduceByKey(lambda x,y: int(x)+int(y))
 # our implementation of algorithm using some spark operations on
 # RDD/iter tools
 
-
 # 2. create dataset of (K, Iterable<V>) pairs
 group = pairs.groupByKey()
 
-# 3. map user id to multiple pairs of [user id + item visited]
+# 3. map user id to multiple pairs of [pair of items visited]
 group_pairs = group.flatMap(lambda items: [(items[0], pair) for pair
                                 in combinations(items[1], 2)])
 
-# return all elements of dataset as an array
-group_output = group_pairs.collect()
+# 4. ((item1, item2), list of user1, user2, etc.)
+co_clicks = group_pairs.groupByKey()
+                        .map(lambda user_list:
+                                (user_list[0],
+                                    set(user_list[1])))
 
+# 5.
+co_clicks_count = co_clicks.map(lambda line: (line[1], len(line[1])))
+
+# 6.
+filtered_items = co_clicks_count.filter(lambda users: users[1] > 2)
+
+# return all elements of dataset as an array
+output = filtered_items.collect()
 ###
 
-output = count.collect()
+# output = count.collect()
+
 for page_id, count in output:
     print ("page_id %s count %d" % (page_id, count))
 print ("Popular items done")
