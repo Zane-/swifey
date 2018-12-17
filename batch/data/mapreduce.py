5ff72d966e9f0ab0c5ac8e4659f035449d3368e6
Zane Bilous <zanebilous@gmail.com>
2018-12-17 18:05:03 -0500

    Start sparkjob automatically and install dependencies

--- batch/data/mapreduce.py
+++ batch/data/mapreduce.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import requests
 
 from collections import defaultdict
--- batch/data/sparkjob.sh
+++ batch/data/sparkjob.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # start spark master
-bin/spark-class org.apache.spark.deploy.master.Master -h spark-master
+bin/spark-class org.apache.spark.deploy.master.Master -h spark-master &
+
+# install dependencies
+apt-get update && apt-get install python3-dev python-pip -y
+pip install requests
 
 # update recommendations every 60 seconds
 while true
